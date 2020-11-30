8c2703e7c644beef88a7a8e940192900504b0ec5
JesusDick <bbcoco55321@gmail.com>
2020-12-01 02:13:08 +0800

    hello

---
+++ test.js
@@ -0,0 +1,15 @@
+export function layout(title, content){
+return``
+}
+
+export function loginUI(){
+    return layout('LogIn', `
+    <h1>ID登入</h1>
+    <form action="/login" method="post">
+    <p><input type="text" placeholder="employee" name="員工姓名"></p>
+    <p><input type="password" placeholder="ID" name="ID"></p>
+    <p><input type="submit" value="Login"></p>
+    </form>
+    ` 
+    )
+}
