00823f4c0b47cbdab13259fcd66068d8ce299157
JesusDick <bbcoco55321@gmail.com>
2020-12-03 22:14:18 +0800

    display

--- posTest.js
+++ posTest.js
@@ -1,11 +1,11 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import * as test from './test.js'
-import { DB } from "https://deno.land/x/sqlite/mod.ts";
+import { DataStore } from "https://deno.land/x/sqlite/mod.ts";
 import { Session } from "https://deno.land/x/session@1.1.0/mod.ts";
 
-const db = new DB("blog.db");
-db.query("CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, title TEXT, body TEXT)");
-db.query("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, email TEXT)");
+const DataStore = new DataStore("pos.DataStore");
+DataStore.query("CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, employee TEXT, title TEXT, body TEXT)");
+DataStore.query("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, employee TEXT, password TEXT, email TEXT)");
 
 const path = new Router()
 
@@ -18,3 +18,9 @@ const posTest = new Application();
 posTest.use(session.use()(session))
 posTest.use(path.routes())
 posTest.use(path.allowedMethods())
+
+async function loginUI(ctx){
+    ctx.response.body = await test.loginUI()
+}
+
+await posTest.listen({ hostname : "127.0.0.1", port : 8000});
