ef03b8c1d7acfe7cd5e223cc953432e3e8a704d4
JesusDick <bbcoco55321@gmail.com>
2020-12-03 13:42:25 +0800

    fix

--- posTest.js
+++ posTest.js
@@ -1,11 +1,11 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import * as test from './test.js'
-import { DB } from "https://deno.land/x/sqlite/mod.ts";
+import { DataStore } from "https://deno.land/x/sqlite/mod.ts";
 import { Session } from "https://deno.land/x/session@1.1.0/mod.ts";
 
-const db = new DB("blog.db");
-db.query("CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, title TEXT, body TEXT)");
-db.query("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, email TEXT)");
+const DataStore = new DataStore("pos.DataStore");
+DataStore.query("CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, title TEXT, body TEXT)");
+DataStore.query("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, email TEXT)");
 
 const path = new Router()
 
