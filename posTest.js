05139779cefcc6a39bd04cd6e5f7ed408e0cb14f
JesusDick <bbcoco55321@gmail.com>
2020-12-03 22:10:02 +0800

    fix

--- posTest.js
+++ posTest.js
@@ -4,8 +4,8 @@ import { DataStore } from "https://deno.land/x/sqlite/mod.ts";
 import { Session } from "https://deno.land/x/session@1.1.0/mod.ts";
 
 const DataStore = new DataStore("pos.DataStore");
-DataStore.query("CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, title TEXT, body TEXT)");
-DataStore.query("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, email TEXT)");
+DataStore.query("CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, employee TEXT, title TEXT, body TEXT)");
+DataStore.query("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, employee TEXT, password TEXT, email TEXT)");
 
 const path = new Router()
 
@@ -18,3 +18,7 @@ const posTest = new Application();
 posTest.use(session.use()(session))
 posTest.use(path.routes())
 posTest.use(path.allowedMethods())
+
+async function loginUI(ctx){
+    ctx.response.body = await test.loginUI()
+}
