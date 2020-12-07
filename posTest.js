afcc5b58eec140cb5283b802699f83bd6c650c3e
JesusDick <bbcoco55321@gmail.com>
2020-12-07 22:54:46 +0800

    fix

--- posTest.js
+++ posTest.js
@@ -1,15 +1,15 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import * as test from './test.js'
-import { DataStore } from "https://deno.land/x/sqlite/mod.ts";
+import { DB } from "https://deno.land/x/sqlite/mod.ts";
 import { Session } from "https://deno.land/x/session@1.1.0/mod.ts";
 
-const DataStore = new DataStore("pos.DataStore");
-DataStore.query("CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, employee TEXT, title TEXT, body TEXT)");
-DataStore.query("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, employee TEXT, password TEXT, email TEXT)");
+const dataStore = new DB("pos.dataStore");
+dataStore.query("CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, employee TEXT, title TEXT, body TEXT)");
+dataStore.query("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, employee TEXT, password TEXT, email TEXT)");
 
 const path = new Router()
 
-path.get('/login', loginUI)
+path.get('/', loginUI)
     .post('/login', )
 
 const session = new Session({ framework: "oak" });
