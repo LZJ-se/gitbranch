37fc94c511cf2b8287fdf644ba3f5b3af50f8583
JesusDick <bbcoco55321@gmail.com>
2020-12-09 21:28:31 +0800

    reset TestA

--- posTest.js
+++ posTest.js
@@ -1,15 +1,16 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import * as test from './test.js'
-import { DataStore } from "https://deno.land/x/sqlite/mod.ts";
+import { DB } from "https://deno.land/x/sqlite/mod.ts";
 import { Session } from "https://deno.land/x/session@1.1.0/mod.ts";
 
-const DataStore = new DataStore("pos.DataStore");
-DataStore.query("CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, employee TEXT, title TEXT, body TEXT)");
-DataStore.query("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, employee TEXT, password TEXT, email TEXT)");
+const dataStore = new DB("pos.dataStore");
+dataStore.query("CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, employee TEXT, title TEXT, body TEXT)");
+dataStore.query("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, employee TEXT, password TEXT, email TEXT)");
 
 const path = new Router()
 
-path.get('/login', test.loginUI)
+path.get('/', loginUI)
+    .post('/login', )
 
 const session = new Session({ framework: "oak" });
 await session.init();
@@ -22,3 +23,7 @@ posTest.use(path.allowedMethods())
 async function loginUI(ctx){
     ctx.response.body = await test.loginUI()
 }
+
+console.log('Server run at http://127.0.0.1:8000')
+
+await posTest.listen({ hostname : "127.0.0.1", port : 8000});
