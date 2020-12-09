de41902cfa0fa108712a6ccda425723d4bada8cf
JesusDick <bbcoco55321@gmail.com>
2020-12-09 21:15:57 +0800

    action change

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ export function layout(title, content){
     export function loginUI(){
         return layout('LogIn', `
         <h1>ID登入</h1>
-        <form action="/login" method="post">
+        <form action="/" method="post">
         <p><input type="text" placeholder="employee" name="員工姓名"></p>
         <p><input type="password" placeholder="ID" name="ID"></p>
             <p><input type="submit" value="Login"></p>
